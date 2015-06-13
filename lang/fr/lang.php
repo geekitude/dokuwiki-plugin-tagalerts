54c2b9d2ea229a4f1c5b85244d63ef1419c1d49e
Simon DELAGE <simon.geekitude@gmail.com>
2015-06-13 02:51:59 +0200

    Added french translation

---
+++ lang/fr/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * French language file for Tag Alerts plugin
+ * 
+ * @author: Simon Delage <simon.geekitude@gmail.com>
+ * @license: CC Attribution-Share Alike 3.0 Unported <http://creativecommons.org/licenses/by-sa/3.0/>
+ */
+
+$lang['description']            = 'Ce plugin est fait pour afficher des alertes lorsque certains tags définis sont détectés. Utilisez ce fichier pour définir des message spécifiques pour certains tags (un tag et un message par ligne).';
+$lang['tagalerts']              = 'Cette page a été marquée comme ';
+$lang['tag_required']           = 'Le plugin Tag est requis pour que Tag Alerts soit d\'une quelconque utilité (https://www.dokuwiki.org/plugin:tag).';
---
+++ lang/fr/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * French settings file for Tag Alerts plugin
+ * 
+ * @author: Simon Delage <simon.geekitude@gmail.com>
+ * @license: CC Attribution-Share Alike 3.0 Unported <http://creativecommons.org/licenses/by-sa/3.0/>
+ */
+
+$lang['inline']                 = 'Type d\'alertes [défaut: <code>messages</code>]';
+$lang['error']                  = 'Liste de tags séparés par une virgule qui déclencheront une alerte basée sur les messages système d\'erreur.';
+$lang['info']                   = 'Liste de tags séparés par une virgule qui déclencheront une alerte basée sur les messages système d\'information.';
+$lang['success']                = 'Liste de tags séparés par une virgule qui déclencheront une alerte basée sur les messages système de succès.';
+$lang['notify']                 = 'Liste de tags séparés par une virgule qui déclencheront une alerte basée sur les messages système de notification..';
